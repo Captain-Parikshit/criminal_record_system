3a66a8623824e4d2dbb407946bd7ddf06ebde218
captain-parikshit <parikshitdoye@gmail.com>
2026-02-04 16:23:59 +0530

    final commit may be

--- backend/server.js
+++ backend/server.js
@@ -68,9 +68,9 @@ app.put("/api/crime/resolve/:id", async (req, res) => {
 
 // MongoDB connection
 mongoose.connect('mongodb://127.0.0.1:27017/criminalDB')
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.log('❌ Mongo Error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Mongo Error:', err));
 
 // Server start
 const PORT = 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
